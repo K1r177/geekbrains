9976a139d97255cf4704dd798a8d84eb5c8b1fd7
K1r177 <K1r177@yandex.ru>
2019-05-06 01:00:45 +0300

    ДЗ к 5уроку, БД тоже прикрепил

---
+++ public/gallery.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../config/config.php';
+
+
+//var_dump(scandir(WWW_DIR . IMG_DIR));
+
+
+
+//$gallery = galleryDraw(IMG_DIR);
+$gallery = createGallery();
+
+
+echo render(TEMPLATES_DIR . 'index.tpl', [
+	'title' => 'Галерея',
+	'h1' => 'Галерея',
+	'content' => $gallery 
+]);
---
+++ templates/gallery_item.tpl
@@ -0,0 +1,6 @@
+<div style="display:inline-block;">
+	<a href="galleryimg.php?id={{ID}}">
+		<img style="width: 200px;" src="{{SRC}}" alt="" />
+	</a>
+	<span>Количество просмотров: {{COUNT_VIEW}}</span>
+</div><br>
